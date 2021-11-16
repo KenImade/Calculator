49277ce0e31e7c0d85b1fd13c5e2e269ff96a702
Kenneth Imade <kenneth.imade@yahoo.com>
2021-11-16 13:08:39 +0100

    created a web based calculator

---
+++ app.js
@@ -0,0 +1,163 @@
+class Calculator {
+    constructor(currentOperandTextElement, previousOperandTextElement){
+        this.currentOperandTextElement = currentOperandTextElement
+        this.previousOperandTextElement = previousOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.previousOperand = ''
+        this.currentOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
+    }
+
+    appendNumber(number) {
+        if(number === '.' && this.currentOperand.includes('.')) return 
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation){
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            default:
+                return    
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber (number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.textContent = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.textContent = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.textContent = ''
+        }
+        
+    }
+
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const deleteButton = document.querySelector('[data-delete]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+window.addEventListener('keydown', handleKeyboardInput)
+
+// function loge (e) {
+//     console.log(e.key)
+// }
+
+
+const calculator = new Calculator(currentOperandTextElement, previousOperandTextElement)
+
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.textContent)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.textContent)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+function handleKeyboardInput(e) {
+    if ((e.key >= 0 && e.key <= 9) || (e.key === '.')) {
+        calculator.appendNumber(e.key)
+        calculator.updateDisplay()
+    }
+    if (e.key === '=' || e.key === 'Enter') {
+        calculator.compute()
+        calculator.updateDisplay()
+    }
+    if (e.key === 'Backspace') {
+        calculator.delete()
+        calculator.updateDisplay()
+    }
+    if (e.key === 'Escape') {
+        calculator.clear()
+        calculator.updateDisplay()
+    }
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        calculator.chooseOperation(convertOperator(e.key))
+        calculator.updateDisplay()
+    }
+}
+  
+function convertOperator(keyboardOperator) {
+if (keyboardOperator === '/') return '÷'
+if (keyboardOperator === '*') return '×'
+if (keyboardOperator === '-') return '−'
+if (keyboardOperator === '+') return '+'
+}
